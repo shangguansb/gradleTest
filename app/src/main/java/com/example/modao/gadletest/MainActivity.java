a5d870ae1feead23f4f89dbfc86f5e2a63e0f4e7
莫道 <modao@mogujie.com>
2016-08-18 12:32:38 +0800

    init

---
+++ app/src/main/java/com/example/modao/gadletest/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.modao.gadletest;
+
+import android.os.Messenger;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView web = new WebView(this);
+
+    }
+
+}
---
+++ modaolib/src/main/java/com/example/modaolib/test.java
@@ -0,0 +1,12 @@
+package com.example.modaolib;
+
+import android.util.Log;
+
+/**
+ * Created by modao on 16/8/18.
+ */
+public class test {
+    public static void print() {
+        Log.e("----------", "modaoLIB");
+    }
+}
